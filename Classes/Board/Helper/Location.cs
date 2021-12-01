b7ec4ab73990120aea67e8c32544ce4c45d2488a
duydang2412 <duydang2412@gmail.com>
2021-12-01 19:47:27 +0700

    refactor(BoardHelper): correct division operand

--- Classes/Board/Helper/Location.cs
+++ Classes/Board/Helper/Location.cs
@@ -2,8 +2,8 @@ using System.Drawing;
 namespace ChineseChess {
     partial class BoardHelper {
         public static PointF GetLocation(Board board, int row, int col) {
-            float cellX = board.Width / (col - 1);
-            float cellY = board.Height / (row - 1);
+            float cellX = board.Width / (Board.Cols - 1);
+            float cellY = board.Height / (Board.Rows - 1);
             return new PointF(board.Location.X + cellX * col, board.Location.Y + cellY * row);
         }
     }
