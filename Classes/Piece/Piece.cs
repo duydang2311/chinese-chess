0400069add18bcc655826e2c6cfddf43e6c2b0bf
duydang2412 <duydang2412@gmail.com>
2021-12-01 19:45:14 +0700

    feat(Piece): add width, height, Draw()

--- Classes/Piece/Piece.cs
+++ Classes/Piece/Piece.cs
@@ -2,6 +2,8 @@ using System.Drawing;
 
 namespace ChineseChess {
     class Piece {
+        public const float Width = 50f;
+        public const float Height = 50f;
         private Pieces type;
         private Player player;
         private Point location;
@@ -20,6 +22,10 @@ namespace ChineseChess {
         }
         public Image Image {
             get => PieceHelper.GetImage(player.Color, this.type);
+        }
+        public void Draw(Color color, Graphics graphics) {
+            PointF location = BoardHelper.GetLocation(this.player.Board, this.location.X, this.location.Y);
+            graphics.DrawImage(PieceHelper.GetImage(color, this.type), location.X, location.Y, Piece.Width, Piece.Height);
         }
     }
 }
