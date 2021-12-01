aeb8671cb02c039e8674ddafcfe351e0e29ecf88
duydang2412 <duydang2412@gmail.com>
2021-12-01 21:43:30 +0700

    style(MainForm): remove unused using - format code style

--- Forms/MainForm/MainForm.cs
+++ Forms/MainForm/MainForm.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
-namespace ChineseChess
-{
-	public partial class MainForm : Form
-	{
-		public MainForm()
-		{
+namespace ChineseChess {
+	public partial class MainForm : Form {
+		public MainForm() {
 			InitializeComponent();
 		}
 	}
