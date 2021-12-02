956effe1e5b1859ff3852260324f39cbc900c863
duydang2412 <duydang2412@gmail.com>
2021-12-02 20:36:25 +0700

    feat(Game): update class Game structure - setup board on construction

--- Classes/Game/Game.cs
+++ Classes/Game/Game.cs
@@ -5,22 +5,31 @@ using System.Windows.Forms;
 namespace ChineseChess {
 	class Game : PictureBox {
 		private Panel panel;
+		private Player[] sidePlayers;
+		private Color[] sideColors;
 		private Board board;
-		public Game(Panel panel, Board board) : base() {
+		public Game(Panel panel, Player[] sidePlayers, Color[] sideColors, Board board) : base() {
 			this.panel = panel;
+			this.sidePlayers = sidePlayers;
+			this.sideColors = sideColors;
 			this.board = board;
 			this.BackColor = System.Drawing.Color.WhiteSmoke;
 			this.ClientSize = new Size(panel.ClientSize.Width, panel.ClientSize.Height);
 			panel.Controls.Add(this);
 			panel.Resize += Panel_Resize;
+
+			this.board.Setup();
 		}
 		public Board Board {
 			get => this.board;
 			set { this.board = value; }
 		}
 		protected override void OnPaint(PaintEventArgs e) {
+			e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+			e.Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
 			base.OnPaint(e);
-			board.Draw(e.Graphics);
+			board.Draw(this.sideColors, e.Graphics);
 		}
 		private void Panel_Resize(object sender, EventArgs e) {
 			this.ClientSize = new Size(panel.ClientSize.Width, panel.ClientSize.Height);
