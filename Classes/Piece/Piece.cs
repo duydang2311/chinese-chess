96ac77e7275aa06187cae5384726850713270b59
duydang2412 <duydang2412@gmail.com>
2021-12-02 20:00:59 +0700

    refactor(Piece): remove player, add board

--- Classes/Piece/Piece.cs
+++ Classes/Piece/Piece.cs
@@ -4,13 +4,15 @@ namespace ChineseChess {
     class Piece {
         public const float Width = 50f;
         public const float Height = 50f;
+        private Board board;
         private Pieces type;
-        private Player player;
         private Point location;
-        public Piece(Player player, Pieces type) {
-            this.player = player;
+        public Piece(Board board, Pieces type) {
             this.type = type;
             this.location = new Point(0, 0);
+        }
+        public Board Board {
+            get => this.board;
         }
         public Pieces Type {
             get => this.type;
@@ -20,11 +22,8 @@ namespace ChineseChess {
             get => this.location;
             set { this.location = value; }
         }
-        public Image Image {
-            get => PieceHelper.GetImage(player.Color, this.type);
-        }
         public void Draw(Color color, Graphics graphics) {
-            PointF location = BoardHelper.GetLocation(this.player.Board, this.location.X, this.location.Y);
+            PointF location = BoardHelper.GetLocation(this.board, this.location.X, this.location.Y);
             graphics.DrawImage(PieceHelper.GetImage(color, this.type), location.X, location.Y, Piece.Width, Piece.Height);
         }
     }
