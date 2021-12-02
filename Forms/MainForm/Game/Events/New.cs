a712b0c337c5f1bec2d3d46e81626fe2a73d8a41
duydang2412 <duydang2412@gmail.com>
2021-12-02 20:36:46 +0700

    feat(MainForm): properly create new game

--- Forms/MainForm/Game/Events/New.cs
+++ Forms/MainForm/Game/Events/New.cs
@@ -6,11 +6,23 @@ namespace ChineseChess {
 	public partial class MainForm : Form {
 		private void Game_OnNew(object sender, EventArgs e) {
 			GameForm form = GameForm.Prompt(this);
-			if(this.game != null) {
-				this.Controls.Remove(this.game);
-				this.game.Dispose();
+			switch(form.DialogResult) {
+				case DialogResult.OK: {
+					if(this.game != null) {
+						this.Controls.Remove(this.game);
+						this.game.Dispose();
+					}
+					int sides = Enum.GetNames(typeof(Side)).Length;
+					Color[] sideColors = new Color[sides];
+					Player[] sidePlayers = new Player[sides];
+					sidePlayers[(int)form.BlackSide] = new Player(form.BlackName, form.BlackIsAI);
+					sidePlayers[(int)form.RedSide] = new Player(form.RedName, form.RedIsAI);
+					sideColors[(int)form.BlackSide] = Color.Black; 
+					sideColors[(int)form.RedSide] = Color.Red; 
+					this.game = new Game(this.panelGame, sidePlayers, sideColors, new Board(this.panelGame.Size.Width * 0.8f, new PointF(this.panelGame.Size.Width * 0.2f / 2, this.panelGame.Size.Height * 0.2f / 2)));
+					break;
+				}
 			}
-			this.game = new Game(this.panelGame, new Board(this.panelGame.Size.Width * 0.8f, new PointF(this.panelGame.Size.Width * 0.2f / 2, this.panelGame.Size.Height * 0.2f / 2)));
 		}
 	}
 }
