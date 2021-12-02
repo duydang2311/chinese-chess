e73ada0855fefcfaec31ef3462b1c432b9c92d48
duydang2412 <duydang2412@gmail.com>
2021-12-02 17:31:23 +0700

    feat(Game): handle panel resize

--- Classes/Game/Game.cs
+++ Classes/Game/Game.cs
@@ -1,15 +1,32 @@
+using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace ChineseChess {
 	class Game : PictureBox {
+		private Panel panel;
 		private Board board;
-		public Game(Board board) : base() {
+		public Game(Panel panel, Board board) : base() {
+			this.panel = panel;
 			this.board = board;
-			MessageBox.Show(this.board.Width + " " + this.board.Height);
+			this.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.ClientSize = new Size(panel.ClientSize.Width, panel.ClientSize.Height);
+			panel.Controls.Add(this);
+			panel.Resize += Panel_Resize;
 		}
 		protected override void OnPaint(PaintEventArgs e) {
 			base.OnPaint(e);
 			board.Draw(e.Graphics);
+		}
+		private void Panel_Resize(object sender, EventArgs e) {
+			this.ClientSize = new Size(panel.ClientSize.Width, panel.ClientSize.Height);
+			if(panel.ClientSize.Width * Board.WidthHeightRatio > panel.ClientSize.Height) {
+				this.board.Height = this.panel.ClientSize.Height * 0.8f;
+			} else {
+				this.board.Width = this.panel.ClientSize.Width * 0.8f;
+			}
+			this.board.Location = new PointF(panel.ClientSize.Width * 0.2f / 2, panel.ClientSize.Height * 0.2f / 2);
+			this.Refresh();
 		}
 	}
 }
