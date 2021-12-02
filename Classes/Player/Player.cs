f1c602ba3f41d09d2105ade8c2f92e68e322f16c
duydang2412 <duydang2412@gmail.com>
2021-12-02 19:58:58 +0700

    refactor(Player): remove Color, pieces - add isAI

--- Classes/Player/Player.cs
+++ Classes/Player/Player.cs
@@ -2,41 +2,15 @@ using System.Collections.Generic;
 using System.Drawing;
 namespace ChineseChess {
     class Player {
-        private Board board;
         private string name;
-        private Color color;
-        private List<Piece> pieces;
-        public Player(Board board, string name) {
-            this.board = board;
+        private bool isAI;
+        public Player(string name, bool isAI) {
             this.name = name;
-            this.pieces = new List<Piece>();
-        }
-        public Board Board {
-            get => this.board;
+            this.isAI = isAI;
         }
         public string Name {
             get => this.name;
             set { this.name = value; }
-        }
-        public Color Color {
-            get => this.color;
-            set { this.color = value; }
-        }
-        public List<Piece> Pieces {
-            get => this.pieces;
-        }
-        public void AddPiece(Piece piece) {
-            this.pieces.Add(piece);
-        }
-        public void RemovePiece(Piece piece) {
-            int index = this.pieces.IndexOf(piece);
-            if(index == -1) return;
-            this.pieces.RemoveAt(index);
-        }
-        public void Draw(Graphics graphics) {
-            foreach(Piece piece in this.pieces) {
-                piece.Draw(this.color, graphics);
-            }
         }
     }
 }
