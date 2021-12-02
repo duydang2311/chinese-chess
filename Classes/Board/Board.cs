1276f408e3a1d779f886d5becfcf6c5c62f52fb2
duydang2412 <duydang2412@gmail.com>
2021-12-02 20:35:52 +0700

    feat(Board): add Board.Setup()

--- Classes/Board/Board.cs
+++ Classes/Board/Board.cs
@@ -33,6 +33,29 @@ namespace ChineseChess {
             get => this.location;
             set { this.location = value; }
         }
+        public void Setup() {
+            int i = 0;
+            foreach(List<Piece> list in sides) {
+                list.Clear();
+                list.Add(new Piece(this, Pieces.General, new Point(i * (Board.Rows - 1), (int)(Board.Cols / 2))));
+                list.Add(new Piece(this, Pieces.Guard, new Point(i * (Board.Rows - 1), (int)(Board.Cols / 2) - 1)));
+                list.Add(new Piece(this, Pieces.Guard, new Point(i * (Board.Rows - 1), (int)(Board.Cols / 2) + 1)));
+                list.Add(new Piece(this, Pieces.Elephant, new Point(i * (Board.Rows - 1), (int)(Board.Cols / 2) - 2)));
+                list.Add(new Piece(this, Pieces.Elephant, new Point(i * (Board.Rows - 1), (int)(Board.Cols / 2) + 2)));
+                list.Add(new Piece(this, Pieces.Horse, new Point(i * (Board.Rows - 1), (int)(Board.Cols / 2) - 3)));
+                list.Add(new Piece(this, Pieces.Horse, new Point(i * (Board.Rows - 1), (int)(Board.Cols / 2) + 3)));
+                list.Add(new Piece(this, Pieces.Chariot, new Point(i * (Board.Rows - 1), (int)(Board.Cols / 2) - 4)));
+                list.Add(new Piece(this, Pieces.Chariot, new Point(i * (Board.Rows - 1), (int)(Board.Cols / 2) + 4)));
+                list.Add(new Piece(this, Pieces.Cannon, new Point(i * (Board.Rows - 1) + ((i == 0) ? 1 : -1) * 2, (int)(Board.Cols / 2) - 3)));
+                list.Add(new Piece(this, Pieces.Cannon, new Point(i * (Board.Rows - 1) + ((i == 0) ? 1 : -1) * 2, (int)(Board.Cols / 2) + 3)));
+                list.Add(new Piece(this, Pieces.Soldier, new Point(i * (Board.Rows - 1) + ((i == 0) ? 1 : -1) * 3, (int)(Board.Cols / 2) - 4)));
+                list.Add(new Piece(this, Pieces.Soldier, new Point(i * (Board.Rows - 1) + ((i == 0) ? 1 : -1) * 3, (int)(Board.Cols / 2) - 2)));
+                list.Add(new Piece(this, Pieces.Soldier, new Point(i * (Board.Rows - 1) + ((i == 0) ? 1 : -1) * 3, (int)(Board.Cols / 2))));
+                list.Add(new Piece(this, Pieces.Soldier, new Point(i * (Board.Rows - 1) + ((i == 0) ? 1 : -1) * 3, (int)(Board.Cols / 2) + 2)));
+                list.Add(new Piece(this, Pieces.Soldier, new Point(i * (Board.Rows - 1) + ((i == 0) ? 1 : -1) * 3, (int)(Board.Cols / 2) + 4)));
+                ++i;
+            }
+        }
         public void AddPiece(Side side, Piece piece) {
             this.sides[(int)side].Add(piece);
         }
@@ -43,11 +66,6 @@ namespace ChineseChess {
         }
         public void Draw(Color[] colors, Graphics graphics) {
             graphics.DrawImage(ChineseChess.Properties.Resources.board_trans, this.location.X, this.location.Y, this.Width, this.Height);
-			for(int i = 0; i != Board.Rows; ++i) {
-				for(int j = 0; j != Board.Cols; ++j) {
-					graphics.DrawEllipse(new Pen(System.Drawing.Color.Red), BoardHelper.GetLocation(this, i, j).X, BoardHelper.GetLocation(this, i, j).Y, 10, 10);
-				}
-			}
             for(int i = 0 ; i != this.sides.Length; ++i) {
                 foreach(Piece piece in this.sides[i]) {
                     piece.Draw(colors[i], graphics);
