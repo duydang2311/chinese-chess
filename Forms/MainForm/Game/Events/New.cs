2e9d6f18b8f13811aa57f8a01e51ccec8086a705
duydang2412 <duydang2412@gmail.com>
2021-12-02 17:31:43 +0700

    feat(MainForm): show GameForm on new game

--- Forms/MainForm/Game/Events/New.cs
+++ Forms/MainForm/Game/Events/New.cs
@@ -5,13 +5,12 @@ using System.Drawing;
 namespace ChineseChess {
 	public partial class MainForm : Form {
 		private void Game_OnNew(object sender, EventArgs e) {
+			GameForm form = GameForm.Prompt(this);
 			if(this.game != null) {
 				this.Controls.Remove(this.game);
 				this.game.Dispose();
 			}
-			this.game = new Game(new Board(400, new PointF(5, 5)));
-			this.game.ClientSize = new Size(this.ClientSize.Width, this.ClientSize.Height);
-			this.panelGame.Controls.Add(this.game);
+			this.game = new Game(this.panelGame, new Board(this.panelGame.Size.Width * 0.8f, new PointF(this.panelGame.Size.Width * 0.2f / 2, this.panelGame.Size.Height * 0.2f / 2)));
 		}
 	}
 }
