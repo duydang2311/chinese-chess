1c22b535daf72f3977ec828297a8d968b890d226
duydang2412 <duydang2412@gmail.com>
2021-12-03 19:43:09 +0700

    feat(Board): add MouseClick for selecting piece

---
+++ Classes/Board/MouseClick.cs
@@ -0,0 +1,24 @@
+using System.Drawing;
+
+namespace ChineseChess {
+    partial class Board {
+        Piece selectedPiece;
+        public Piece SelectedPiece {
+            get => this.selectedPiece;
+            set {
+                if(this.selectedPiece != null) {
+                    this.selectedPiece.Highlight = Color.Transparent;
+                }
+                this.selectedPiece = value;
+                if(value != null) {
+                    this.selectedPiece.Highlight = Color.Green;
+                }
+            }
+        }
+        public bool OnMouseClick() {
+            Piece before = this.selectedPiece;
+            this.SelectedPiece = this.highlightedPiece;
+            return (before != this.selectedPiece);
+        }
+    }
+}
