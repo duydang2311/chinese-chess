38c94547613808d6779973b9deaffd83f314c64b
duydang2412 <duydang2412@gmail.com>
2021-12-03 18:26:10 +0700

    feat(Board): make use of return to/to not update

--- Classes/Board/MouseMove.cs
+++ Classes/Board/MouseMove.cs
@@ -1,16 +1,28 @@
-using System.Windows.Forms;
+using System.Drawing;
 
 namespace ChineseChess {
     partial class Board {
-        public void OnMouseMove(float x, float y) {
+        private Piece highlightPiece;
+        public bool OnMouseMove(float x, float y) {
             for(int i = 0 ; i != this.sides.Length; ++i) {
                 foreach(Piece piece in this.sides[i]) {
                     if(piece.IsPointCollided(x, y)) {
+                        if(piece == highlightPiece) return false;
+                        if(highlightPiece != null) {
+                            highlightPiece.Highlight = Color.Transparent;
+                        }
                         piece.Highlight = System.Drawing.Color.Red;
-                        break;
+                        highlightPiece = piece;
+                        return true;
                     }
                 }
             }
+            if(highlightPiece != null) {
+                highlightPiece.Highlight = Color.Transparent;
+                highlightPiece = null;
+                return true;
+            }
+            return false;
         }
     }
 }
