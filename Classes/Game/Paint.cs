6712c58e4469b52d16975f4b7cdba30b38daa924
duydang2412 <duydang2412@gmail.com>
2021-12-03 20:39:46 +0700

    refactor(Game): adjust graphics InterpolationMode

--- Classes/Game/Paint.cs
+++ Classes/Game/Paint.cs
@@ -4,8 +4,8 @@ using System.Windows.Forms;
 namespace ChineseChess {
     partial class Game : PictureBox {
 		protected override void OnPaint(PaintEventArgs e) {
-			e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
-			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+			e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Bilinear;
+			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
 			e.Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
 			base.OnPaint(e);
 			board.Draw(this.sideColors, e.Graphics);
