1e9e1ad340fb1561f0eb3e4df9274b9cfa5cfe0c
duydang2412 <duydang2412@gmail.com>
2021-12-03 17:36:51 +0700

    feat(Game): extend Dispose()

--- Classes/Game/Game.cs
+++ Classes/Game/Game.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Reflection;
 
 namespace ChineseChess {
 	class Game : PictureBox {
@@ -16,20 +17,13 @@ namespace ChineseChess {
 			this.BackColor = System.Drawing.Color.WhiteSmoke;
 			this.ClientSize = new Size(panel.ClientSize.Width, panel.ClientSize.Height);
 			panel.Controls.Add(this);
-			panel.Resize += Panel_Resize;
+			panel.Resize += this.Panel_Resize;
 
 			this.board.Setup();
 		}
 		public Board Board {
 			get => this.board;
 			set { this.board = value; }
-		}
-		protected override void OnPaint(PaintEventArgs e) {
-			e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
-			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-			e.Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
-			base.OnPaint(e);
-			board.Draw(this.sideColors, e.Graphics);
 		}
 		private void Panel_Resize(object sender, EventArgs e) {
 			this.ClientSize = new Size(panel.ClientSize.Width, panel.ClientSize.Height);
@@ -40,6 +34,28 @@ namespace ChineseChess {
 			}
 			this.board.Location = new PointF(panel.ClientSize.Width * 0.2f / 2, panel.ClientSize.Height * 0.2f / 2);
 			this.Refresh();
+		}
+		protected override void OnPaint(PaintEventArgs e) {
+			e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+			e.Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+			base.OnPaint(e);
+			board.Draw(this.sideColors, e.Graphics);
+		}
+		protected override void Dispose(bool disposing) {
+			if(disposing) {
+				this.panel.Controls.Remove(this);
+				this.panel.Resize -= this.Panel_Resize;
+				this.panel = null;
+                this.sidePlayers = null;
+                this.sideColors = null;
+                this.board = null;
+			}
+			base.Dispose(disposing);
+		}
+		~Game() {
+			MessageBox.Show("did call");
+			this.Dispose(false);
 		}
 	}
 }
