39d70f36f2a813c28533969e753fcb3fca9f5580
duydang2412 <duydang2412@gmail.com>
2021-12-03 20:41:05 +0700

    fix(MainForm): update Board constructor caller

--- Forms/MainForm/Game/Events/New.cs
+++ Forms/MainForm/Game/Events/New.cs
@@ -19,7 +19,7 @@ namespace ChineseChess {
 					sidePlayers[(int)form.RedSide] = new Player(form.RedName, form.RedIsAI);
 					sideColors[(int)form.BlackSide] = SideColor.Black; 
 					sideColors[(int)form.RedSide] = SideColor.Red; 
-					this.game = new Game(this.panelGame, sidePlayers, sideColors, new Board(this.panelGame.Size.Width * 0.8f, new PointF(this.panelGame.Size.Width * 0.2f / 2, this.panelGame.Size.Height * 0.2f / 2)));
+					this.game = new Game(this.panelGame, sidePlayers, sideColors, new Board(ChineseChess.Properties.Resources.board_trans, this.panelGame.Size.Width * 0.8f, new PointF(this.panelGame.Size.Width * 0.2f / 2, this.panelGame.Size.Height * 0.2f / 2)));
 					break;
 				}
 			}
