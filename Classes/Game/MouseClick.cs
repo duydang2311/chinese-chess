fcddcbb9111121e933fda44de4842de56274b66f
duydang2412 <duydang2412@gmail.com>
2021-12-04 19:01:25 +0700

    refactor(Game): add board.OnMouseClick() parameters

--- Classes/Game/MouseClick.cs
+++ Classes/Game/MouseClick.cs
@@ -3,7 +3,7 @@ using System.Windows.Forms;
 namespace ChineseChess {
     partial class Game : PictureBox {
 		protected override void OnMouseClick(MouseEventArgs e) {
-			if(board.OnMouseClick()) {
+			if(board.OnMouseClick(e.Location.X, e.Location.Y)) {
 				this.Refresh();
 			}
 			base.OnMouseClick(e);
