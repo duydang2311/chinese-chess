1720aef1f6ba07bb8eb1bb6683e15a229335ac4d
duydang2412 <duydang2412@gmail.com>
2021-12-04 16:07:13 +0700

    feat(Direction): add Compare()

--- Classes/Direction/Helper.cs
+++ Classes/Direction/Helper.cs
@@ -5,5 +5,8 @@ namespace ChineseChess {
             int y = piece.Location.Y + direction.Y;
             return (x >= 0 && x < Board.Cols) && (y >= 0 && y < Board.Rows);
         }
+        public static bool Compare(Direction a, Direction b) {
+            return (a.X == b.X && a.Y == b.Y);
+        }
     }
 }
