a26f0713e7e5442af76b8b35a427950398845995
duydang2412 <duydang2412@gmail.com>
2021-12-04 13:50:13 +0700

    fix(Board): correct GetLocation() parameter order

--- Classes/Board/Helper/Location.cs
+++ Classes/Board/Helper/Location.cs
@@ -1,7 +1,7 @@
 using System.Drawing;
 namespace ChineseChess {
     static partial class BoardHelper {
-        public static PointF GetLocation(Board board, int row, int col) {
+        public static PointF GetLocation(Board board, int col, int row) {
             float cellX = board.Width / (Board.Cols - 1);
             float cellY = board.Height / (Board.Rows - 1);
             return new PointF(board.Location.X + cellX * col, board.Location.Y + cellY * row);
