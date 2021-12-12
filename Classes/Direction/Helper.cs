806ddc838d8bdb70568c3977029b22da567540a7
duydang2412 <duydang2412@gmail.com>
2021-12-13 01:03:14 +0700

    feat(Direction): add Normalize() to Helper class

--- Classes/Direction/Helper.cs
+++ Classes/Direction/Helper.cs
@@ -1,4 +1,5 @@
 namespace ChineseChess {
+    using System;
     static class DirectionHelper {
         public static bool Validate(Piece piece, Direction direction) {
             int x = piece.Location.X + direction.X;
@@ -7,6 +8,12 @@ namespace ChineseChess {
         }
         public static bool Compare(Direction a, Direction b) {
             return (a.X == b.X && a.Y == b.Y);
+        }
+        public static Direction Normalize(int x, int y) {
+            float length = (float)Math.Sqrt(x * x + y * y);
+            x = (int)(x / length);
+            y = (int)(y / length);
+            return new Direction(x, y);
         }
     }
 }
