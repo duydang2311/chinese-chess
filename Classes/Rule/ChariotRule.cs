5290211a6e05a372c5c2328304bcccc3cbf44843
duydang2412 <duydang2412@gmail.com>
2021-12-13 01:02:42 +0700

    feat(ChariotRule): implement GetMoves()

--- Classes/Rule/ChariotRule.cs
+++ Classes/Rule/ChariotRule.cs
@@ -1,28 +1,12 @@
 namespace ChineseChess {
     using System.Collections.Generic;
     static class ChariotRule {
-        public static List<Direction> GetMoves(Side side, Piece piece) {
+        public static List<Direction> GetMoves(Side side, Piece piece, List<Piece> pieces) {
             List<Direction> list = new List<Direction>();
-            Direction direction = Direction.Forward(side);
-            while(DirectionHelper.Validate(piece, direction)) {
-                list.Add(direction);
-                direction = direction + Direction.Forward(side);
-            }
-            direction = Direction.Backward(side);
-            while(DirectionHelper.Validate(piece, direction)) {
-                list.Add(direction);
-                direction = direction + Direction.Backward(side);
-            }
-            direction = Direction.Left(side);
-            while(DirectionHelper.Validate(piece, direction)) {
-                list.Add(direction);
-                direction = direction + Direction.Left(side);
-            }
-            direction = Direction.Right(side);
-            while(DirectionHelper.Validate(piece, direction)) {
-                list.Add(direction);
-                direction = direction + Direction.Right(side);
-            }
+            list.AddRange(RuleHelper.GetMoves(piece, pieces, Direction.Forward(side)));
+            list.AddRange(RuleHelper.GetMoves(piece, pieces, Direction.Backward(side)));
+            list.AddRange(RuleHelper.GetMoves(piece, pieces, Direction.Left(side)));
+            list.AddRange(RuleHelper.GetMoves(piece, pieces, Direction.Right(side)));
             return list;
         }
     }
