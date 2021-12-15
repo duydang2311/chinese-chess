bdd42614860c7710a389bfbbf206b6762e3773b9
duydang2412 <duydang2412@gmail.com>
2021-12-15 16:24:52 +0700

    feat(Board): integrate Piece.Capture() into GuideClick

--- Classes/Board/GuideClick.cs
+++ Classes/Board/GuideClick.cs
@@ -2,7 +2,8 @@ namespace ChineseChess {
     partial class Board {
         private void OnGuideClick(object sender, GuideClickEventArgs e) {
             if(this.selectedPiece == null) return;
-            this.selectedPiece.Location = new System.Drawing.Point(e.X, e.Y);
+            this.selectedPiece.Capture(e.X, e.Y);
+            this.HighlightedPiece = null; // workaround for MouseClick
         }
     }
 }
