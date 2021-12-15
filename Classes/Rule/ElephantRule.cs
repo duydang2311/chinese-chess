dff6d46bc248640f2001208a554c11ce8d9ba9eb
duydang2412 <duydang2412@gmail.com>
2021-12-15 14:49:16 +0700

    feat(ElephantRule): correct elephant's rule not crossing the river

--- Classes/Rule/ElephantRule.cs
+++ Classes/Rule/ElephantRule.cs
@@ -1,16 +1,47 @@
 namespace ChineseChess {
     using System.Collections.Generic;
     static class ElephantRule {
+        private static List<Direction> GetMoves(Piece piece, List<Piece> pieces, Direction direction, Direction restricted, int steps = -1) {
+            List<Direction> moves = new List<Direction>();
+            int x = piece.Location.X;
+            int y = piece.Location.Y;
+            Piece blockPiece = null;
+            Direction block = new Direction(11, 11);
+            Direction temp;
+            foreach(Piece p in pieces) {
+                temp = new Direction(p.Location.X - x, p.Location.Y - y);
+                if(DirectionHelper.Compare(temp, restricted)) {
+                    return moves;
+                }
+                if(DirectionHelper.Compare(direction.Normalized, temp.Normalized)) {
+                    if(temp.Length < block.Length) {
+                        block = temp;
+                        blockPiece = p;
+                    }
+                }
+            }
+            if(blockPiece is not null && blockPiece.Side != piece.Side) {
+                block += direction;
+            }
+            temp = direction;
+            Direction min = new Direction(0, (Board.Rows - 5) * (int)piece.Side);
+            Direction max = new Direction(Board.Cols + 1, (Board.Rows - 5) * (int)piece.Side + 5);
+            while(steps-- != 0 && DirectionHelper.Validate(piece, temp, min, max) && !DirectionHelper.Compare(temp, block)) {
+                moves.Add(temp);
+                temp += direction;
+            }
+            return moves;
+        }
         public static List<Direction> GetMoves(Piece piece, List<Piece> pieces) {
             List<Direction> list = new List<Direction>();
             Direction fl = Direction.Forward(piece.Side) + Direction.Left(piece.Side);
             Direction fr = Direction.Forward(piece.Side) + Direction.Right(piece.Side);
             Direction bl = Direction.Backward(piece.Side) + Direction.Left(piece.Side);
             Direction br = Direction.Backward(piece.Side) + Direction.Right(piece.Side);
-            list.AddRange(RuleHelper.GetMoves(piece, pieces, fl * 2, fl, 1));
-            list.AddRange(RuleHelper.GetMoves(piece, pieces, fr * 2, fr, 1));
-            list.AddRange(RuleHelper.GetMoves(piece, pieces, bl * 2, bl, 1));
-            list.AddRange(RuleHelper.GetMoves(piece, pieces, br * 2, br, 1));
+            list.AddRange(ElephantRule.GetMoves(piece, pieces, fl * 2, fl, 1));
+            list.AddRange(ElephantRule.GetMoves(piece, pieces, fr * 2, fr, 1));
+            list.AddRange(ElephantRule.GetMoves(piece, pieces, bl * 2, bl, 1));
+            list.AddRange(ElephantRule.GetMoves(piece, pieces, br * 2, br, 1));
             return list;
         }
     }
