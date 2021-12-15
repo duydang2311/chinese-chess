d0c825be9ce8d4e1856667617f3ca0646ab17fe5
duydang2412 <duydang2412@gmail.com>
2021-12-16 00:29:13 +0700

    fix(MainForm): no longer use Prompt() for Game_OnNew()

--- Forms/MainForm/Game/Events/New.cs
+++ Forms/MainForm/Game/Events/New.cs
@@ -6,21 +6,23 @@ using System.Threading.Tasks;
 namespace ChineseChess {
 	public partial class MainForm : Form {
 		private void Game_OnNew(object sender, EventArgs e) {
-			GameForm form = GameForm.Prompt(this);
-			switch(form.DialogResult) {
-				case DialogResult.OK: {
-					if(this.game != null) {
-						this.game.Dispose();
+			using(GameForm form = new GameForm()) {
+				switch(form.ShowDialog(this)) {
+					case DialogResult.OK: {
+						if(this.game != null) {
+							this.game.Dispose();
+						}
+						int sides = Enum.GetNames(typeof(Side)).Length;
+						SideColor[] sideColors = new SideColor[sides];
+						Player[] sidePlayers = new Player[sides];
+						sidePlayers[(int)form.BlackSide] = new Player(form.BlackName, form.BlackIsAI);
+						sidePlayers[(int)form.RedSide] = new Player(form.RedName, form.RedIsAI);
+						sideColors[(int)form.BlackSide] = SideColor.Black; 
+						sideColors[(int)form.RedSide] = SideColor.Red; 
+						this.game = new Game(this.panelGame, sidePlayers, sideColors, new Board(ChineseChess.Properties.Resources.board_trans, this.panelGame.Size.Width * 0.8f, new PointF(this.panelGame.Size.Width * 0.2f / 2, this.panelGame.Size.Height * 0.2f / 2)));
+						this.InitStats();
+						break;
 					}
-					int sides = Enum.GetNames(typeof(Side)).Length;
-					SideColor[] sideColors = new SideColor[sides];
-					Player[] sidePlayers = new Player[sides];
-					sidePlayers[(int)form.BlackSide] = new Player(form.BlackName, form.BlackIsAI);
-					sidePlayers[(int)form.RedSide] = new Player(form.RedName, form.RedIsAI);
-					sideColors[(int)form.BlackSide] = SideColor.Black; 
-					sideColors[(int)form.RedSide] = SideColor.Red; 
-					this.game = new Game(this.panelGame, sidePlayers, sideColors, new Board(ChineseChess.Properties.Resources.board_trans, this.panelGame.Size.Width * 0.8f, new PointF(this.panelGame.Size.Width * 0.2f / 2, this.panelGame.Size.Height * 0.2f / 2)));
-					break;
 				}
 			}
 		}
