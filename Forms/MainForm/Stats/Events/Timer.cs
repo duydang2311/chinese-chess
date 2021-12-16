d549037d3c5a30edb7d8dbe765fc795af409835c
duydang2412 <duydang2412@gmail.com>
2021-12-16 21:40:27 +0700

    feat(MainForm/Stats): update time labels on timer elapsed

--- Forms/MainForm/Stats/Events/Timer.cs
+++ Forms/MainForm/Stats/Events/Timer.cs
@@ -4,11 +4,11 @@ namespace ChineseChess {
 	public partial class MainForm : Form {
         private void OnTopTimerElapsed(object sender, ElapsedEventArgs e) {
             if(this.game is null) return;
-            this.topTimeLabel.Text = this.game.SidePlayers[(int)Side.Top].Minute + ":" + (this.game.SidePlayers[(int)Side.Top].Second % 60);
+            this.topTimeLabel.Text = this.game.SidePlayers[(int)Side.Top].Minute.ToString().PadLeft(2, '0') + ":" + (this.game.SidePlayers[(int)Side.Top].Second % 60).ToString().PadLeft(2, '0');
         }
         private void OnBottomTimerElapsed(object sender, ElapsedEventArgs e) {
             if(this.game is null) return;
-            this.bottomTimeLabel.Text = this.game.SidePlayers[(int)Side.Bottom].Minute + ":" + (this.game.SidePlayers[(int)Side.Bottom].Second % 60);
+            this.bottomTimeLabel.Text = this.game.SidePlayers[(int)Side.Bottom].Minute.ToString().PadLeft(2, '0') + ":" + (this.game.SidePlayers[(int)Side.Bottom].Second % 60).ToString().PadLeft(2, '0');
         }
 	}
 }
