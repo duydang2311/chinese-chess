4c7a6fc9b006376c995bf0154ac7927618101471
duydang2412 <duydang2412@gmail.com>
2021-12-16 16:12:50 +0700

    feat(Board): add capturing piece to captured list on guide click

--- Classes/Board/GuideClick.cs
+++ Classes/Board/GuideClick.cs
@@ -2,8 +2,12 @@ namespace ChineseChess {
     partial class Board {
         private void OnGuideClick(object sender, GuideClickEventArgs e) {
             if(this.selectedPiece == null) return;
-            this.selectedPiece.Capture(e.X, e.Y);
-            this.HighlightedPiece = null; // workaround for MouseClick
+            Piece capturing = this.selectedPiece.Capture(e.X, e.Y);
+            this.HighlightedPiece = null; //workaround for MouseClick
+            if(capturing is not null) {
+                return;
+            }
+            this.captured[(int)capturing.Side].Add(capturing);
         }
     }
 }
