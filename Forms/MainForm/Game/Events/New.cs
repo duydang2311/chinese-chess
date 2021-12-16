4d2790deae8781d49fb164b33f1fdc734e7c7904
duydang2412 <duydang2412@gmail.com>
2021-12-17 05:23:38 +0700

    refactor(MainForm): use if

--- Forms/MainForm/Game/Events/New.cs
+++ Forms/MainForm/Game/Events/New.cs
@@ -7,24 +7,20 @@ namespace ChineseChess {
 	public partial class MainForm : Form {
 		private void Game_OnNew(object sender, EventArgs e) {
 			using(GameForm form = new GameForm()) {
-				switch(form.ShowDialog(this)) {
-					case DialogResult.OK: {
-						if(this.game != null) {
-							this.game.Dispose();
-						}
-						int sides = Enum.GetNames(typeof(Side)).Length;
-						SideColor[] sideColors = new SideColor[sides];
-						Player[] sidePlayers = new Player[sides];
-						sidePlayers[(int)form.BlackSide] = new Player(form.BlackName, form.BlackIsAI);
-						sidePlayers[(int)form.RedSide] = new Player(form.RedName, form.RedIsAI);
-						sideColors[(int)form.BlackSide] = SideColor.Black; 
-						sideColors[(int)form.RedSide] = SideColor.Red; 
-						this.game = new Game(this.panelGame, sidePlayers, sideColors, new Board(ChineseChess.Properties.Resources.board_trans, this.panelGame.Size.Width * 0.8f, new PointF(this.panelGame.Size.Width * 0.2f / 2, this.panelGame.Size.Height * 0.2f / 2)));
-						this.game.Board.Game = this.game;
-						this.InitStats();
-						break;
-					}
+				if(form.ShowDialog(this) != DialogResult.OK) return;
+				if(this.game != null) {
+					this.game.Dispose();
 				}
+				int sides = Enum.GetNames(typeof(Side)).Length;
+				SideColor[] sideColors = new SideColor[sides];
+				Player[] sidePlayers = new Player[sides];
+				sidePlayers[(int)form.BlackSide] = new Player(form.BlackName, form.BlackIsAI);
+				sidePlayers[(int)form.RedSide] = new Player(form.RedName, form.RedIsAI);
+				sideColors[(int)form.BlackSide] = SideColor.Black; 
+				sideColors[(int)form.RedSide] = SideColor.Red; 
+				this.game = new Game(this.panelGame, sidePlayers, sideColors, new Board(ChineseChess.Properties.Resources.board_trans, this.panelGame.Size.Width * 0.8f, new PointF(this.panelGame.Size.Width * 0.2f / 2, this.panelGame.Size.Height * 0.2f / 2)));
+				this.game.Board.Game = this.game;
+				this.InitStats();
 			}
 		}
 	}
