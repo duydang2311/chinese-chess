f9cd8f23a9dc2648361108d1d26ca8dc4b15c192
duydang2412 <duydang2412@gmail.com>
2021-12-17 06:25:39 +0700

    feat(Game): draw captured if has enough size

--- Classes/Game/Paint.cs
+++ Classes/Game/Paint.cs
@@ -8,7 +8,15 @@ namespace ChineseChess {
 			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
 			e.Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
 			base.OnPaint(e);
-			board.Draw(this.sideColors, e.Graphics);
+			this.board.Draw(this.sideColors, e.Graphics);
+
+			float cellWidth = this.board.Width / Board.Cols;
+			float cellHeight = this.board.Height / Board.Rows;
+			if(this.panel.Size.Width - this.board.Width - this.board.Location.X > cellWidth * 2 + this.board.Location.X) {
+				this.board.DrawCaptured(this.sideColors, e.Graphics, (int)((this.panel.Size.Width - this.board.Width - this.board.Location.X) / (cellWidth * 2 + this.board.Location.X)) - 1);
+			} else if(this.panel.Size.Height - this.board.Height - this.board.Location.Y > cellHeight * 2 + this.board.Location.Y) {
+				this.board.DrawCaptured(this.sideColors, e.Graphics, (int)((this.panel.Size.Height - this.board.Height - this.board.Location.Y) / (cellHeight * 2 + this.board.Location.Y)) - 1, false);
+			}
 		}
     }
 }
