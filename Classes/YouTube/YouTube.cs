41c5e97721f4dacbb0bd40904f81f9f8e77172a5
duydang2412 <duydang2412@gmail.com>
2021-12-16 22:00:12 +0700

    feat(YouTube): add new api key

--- Classes/YouTube/YouTube.cs
+++ Classes/YouTube/YouTube.cs
@@ -6,21 +6,38 @@ namespace ChineseChess {
     using Google.Apis.YouTube.v3.Data;
     using Google.Apis.YouTube.v3;
     using Google.Apis.Services;
+    using Google.Apis.Requests;
+    using Google;
     static class YouTube 
     {
+        private static readonly string[] ApiKeys = {
+            "AIzaSyC9bfFOOYKMMRXK6oJnxKDco-2Y7-jKyn0",
+            "AIzaSyBReUZ-1lvauRN130H0BnJhgWGsovWloM0"
+        };
+        private static int activeKey = 0;
         public static async Task<SearchListResponse> Search(string q)
         {
-            YouTubeService service = new YouTubeService(new BaseClientService.Initializer
-                {
-                    ApplicationName = "Chinese Chess",
-                    ApiKey="AIzaSyC9bfFOOYKMMRXK6oJnxKDco-2Y7-jKyn0",
-                });
+            try {
+                YouTubeService service = new YouTubeService(new BaseClientService.Initializer
+                    {
+                        ApplicationName = "Chinese Chess",
+                        ApiKey = YouTube.ApiKeys[YouTube.activeKey],
+                    });
 
-            SearchResource.ListRequest req = service.Search.List("id");
-            req.Q = q;
-            req.MaxResults = 1;
-            SearchListResponse res = await req.ExecuteAsync();
-            return res;
+                SearchResource.ListRequest req = service.Search.List("id");
+                req.Q = q;
+                req.MaxResults = 1;
+                SearchListResponse res = await req.ExecuteAsync();
+                return res;
+            } catch(Google.GoogleApiException ex) {
+                if(ex.Error.Code == 403) {
+                    activeKey = (++activeKey) % YouTube.ApiKeys.Length;
+                }
+                return await YouTube.Search(q);
+            } catch(Exception ex) {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+            return new SearchListResponse();
         }
     }
 }
