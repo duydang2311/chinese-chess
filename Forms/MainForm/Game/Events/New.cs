0e0358b9948081efd77d19dca9040b25f6a011c0
duydang2412 <duydang2412@gmail.com>
2021-12-17 13:22:56 +0700

    feat(MainForm/Game): assign sidePlayers.game on new game

--- Forms/MainForm/Game/Events/New.cs
+++ Forms/MainForm/Game/Events/New.cs
@@ -21,6 +21,8 @@ namespace ChineseChess {
 				sideColors[(int)form.RedSide] = SideColor.Red; 
 				this.game = new Game(this.panelGame, sidePlayers, sideColors, new Board(ChineseChess.Properties.Resources.board_trans, this.panelGame.Size.Width * 0.8f, new PointF(this.panelGame.Size.Width * 0.2f / 2, this.panelGame.Size.Height * 0.2f / 2)));
 				this.game.Board.Game = this.game;
+				sidePlayers[(int)Side.Top].Game = this.game;
+				sidePlayers[(int)Side.Bottom].Game = this.game;
 				this.InitStats();
 			}
 		}
