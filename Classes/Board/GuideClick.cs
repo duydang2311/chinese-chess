4d2323058343e6e5fcd5ec4bff07f130b13cf5b0
duydang2412 <duydang2412@gmail.com>
2021-12-17 13:19:31 +0700

    feat(Board): end game and update side point if capturing

--- Classes/Board/GuideClick.cs
+++ Classes/Board/GuideClick.cs
@@ -4,10 +4,15 @@ namespace ChineseChess {
             if(this.selectedPiece == null) return;
             Piece capturing = this.selectedPiece.Capture(e.X, e.Y);
             this.HighlightedPiece = null; //workaround for MouseClick
+            this.game.SwitchTurn();
             if(capturing is null) {
                 return;
             }
+            this.game.UpdateSidePoint(capturing);
             this.captured[(int)capturing.Side].Add(capturing);
+            if(capturing.Type == Pieces.General) {
+                this.game.End();
+            }
         }
     }
 }
